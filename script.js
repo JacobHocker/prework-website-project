61a95ea20d8c992409da3519d23c27810a2a18e5
JacobHocker <jacobdouglas06@gmail.com>
2021-05-15 09:30:02 -0500

    added ability to click sections and have the site take you to the sections

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+(#aboutme).click(function() {
+    ('html, body').animate({
+      scrollTop: ("#aboutme").offset().top
+    }, 1000);  
+    });
 
+    (#animals).click(function() {
+        ('html, body').animate({
+          scrollTop: ("#animals").offset().top
+        }, 1000);  
+        });
+
+        
