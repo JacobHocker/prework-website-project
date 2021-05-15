c76595ceece71e50b85ce0041b26b10bd4e29059
JacobHocker <jacobdouglas06@gmail.com>
2021-05-15 13:49:03 -0500

    added more java finished up the contact me section

--- script.js
+++ script.js
@@ -4,10 +4,27 @@
     }, 1000);  
     });
 
-    (#animals).click(function() {
-        ('html, body').animate({
-          scrollTop: ("#animals").offset().top
-        }, 1000);  
-        });
+(#animals).click(function() {
+    ('html, body').animate({
+        scrollTop: ("#animals").offset().top
+    }, 1000);  
+    });
 
-        
+(#games).click(function() {
+    ('html, body').animate({
+        scrollTop: ("#games").offset().top
+    }, 1000);  
+    });
+
+(#music).click(function() {
+    ('html, body').animate({
+         scrollTop: ("music").offset().top
+     }, 1000);  
+     });
+
+(#contactme).click(function() {
+    ('html, body').animate({
+        scrollTop: ("#contactme").offset().top
+    }, 1000);  
+     });
+
